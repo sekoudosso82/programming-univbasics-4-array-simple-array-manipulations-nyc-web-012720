7ec2dd53b4b9ee8d1304678087a0c90a83f2f534
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 04:05:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,8 +38,11 @@ def using_flatten(array)
   array.flatten 
 end
 
-def using_delete_at(array, element)
-  array.delete_at(element)
+def using_delete(array, element)
+  array.delete(element)
+end 
+def using_delete_at(array, index)
+  array.delete_at(index)
 end 
 
 
